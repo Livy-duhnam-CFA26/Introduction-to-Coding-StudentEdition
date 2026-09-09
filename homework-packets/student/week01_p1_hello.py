48c90ebd9de32db35877c347af44523fa7b45a33
Livy-duhnam-CFA26 <Livy-duhnam_CFA26@proton.me>
2026-09-08 21:36:40 -0500

    Update week01_p1_hello.py

--- homework-packets/student/week01_p1_hello.py
+++ homework-packets/student/week01_p1_hello.py
@@ -1,1 +1,26 @@
 print ("welcome to class!")
+print (" welcome to class!")
+ welcome to class!
+>>> print (" name:olivia")
+ name:olivia
+>>> print ("grade:8")
+grade:8
+>>> print (" goal one: learn to paint")
+ goal one: learn to paint
+>>> print (" goal two: paint with your favorite color")
+ goal two: paint with your favorite color
+>>> print (" goal three: win art show!")
+ goal three: win art show!
+>>> print ("note: it would be good to come up with more of a theme.")
+note: it would be good to come up with more of a theme.
+>>> print (" open terminal")
+ open terminal
+>>> print (" navigate to folder ")
+ navigate to folder 
+>>> print (" run python filename.py")
+ run python filename.py
+>>> print (" read output")
+ read output
+>>> print (" fix errors and rerun")
+ fix errors and rerun
+>>> 
